d613e89c5fe0afe60555d0bdbc8a66210cdcce98
TursunmetovaFeruza <flyfly.99@mail.ru>
2019-04-22 12:51:04 +0600

    first commit

---
+++ app/app.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const Schema = mongoose.Schema;
+const app = express();
+const jsonParser = express.json();
+ 
+const textScheme = new Schema({data: String}, {versionKey: false});
+const text = mongoose.model("texts", textScheme);
+ 
+app.use(express.static('public'))
+
+//app.use(express.static(__dirname + "/public"));
+ 
+mongoose.connect("mongodb://localhost:27017/text", { useNewUrlParser: true }, function(err){
+    if(err) return console.log(err);
+    app.listen(3000, function(){
+        console.log("Сервер ожидает подключения...");
+    });
+});
+  
+app.get("/api/texts", function(req, res){
+        
+    text.find({}, function(err, texts){
+ 
+        if(err) return console.log(err);
+        res.send(texts)
+    });
+});
+ 
+app.get("/api/texts/:id", function(req, res){
+         
+    const id = req.params.id;
+    text.findOne({_id: id}, function(err, text){
+          
+        if(err) return console.log(err);
+        res.send(text);
+    });
+});
+    
+app.post("/api/texts", jsonParser, function (req, res) {
+        
+    if(!req.body) return res.sendStatus(400);
+        
+    const data = req.body.data;
+    console.log(data);
+    const texts = new text({data: data});
+        
+    texts.save(function(err){
+        if(err) return console.log(err);
+        res.send(texts);
+    });
+});
+     
+app.delete("/api/texts/:id", function(req, res){
+         
+    const id = req.params.id;
+    text.findByIdAndDelete(id, function(err, texts){
+                
+        if(err) return console.log(err);
+        res.send(texts);
+    });
+});
+    
+app.put("/api/texts", jsonParser, function(req, res){
+         
+    if(!req.body) return res.sendStatus(400);
+    const id = req.body.id;
+    const data = req.body.data;
+    const newtext = {data: data};
+     
+    text.findOneAndUpdate({_id: id}, newtext, {new: true}, function(err, texts){
+        if(err) return console.log(err); 
+        res.send(texts);
+    });
+});
---
+++ js/style.js
@@ -0,0 +1,75 @@
+	var i=0;
+	var images=[];
+	images=[
+	'../MongoDB/style/img/1.jpg',
+	'../MongoDB/style/img/2.jpg',
+	'../MongoDB/style/img/3.jpg',
+	'../MongoDB/style/img/4.jpg',
+	'../MongoDB/style/img/5.jpg'
+	];
+
+		document.slide.src=images[0];
+	$('section').on('click','#left',function(){
+		var s=0;
+		if(i>images.length-1){
+			i--;
+			s=i+1;
+			document.slide.src=images[i];
+			 document.getElementById("slide-id").innerHTML ="0"+s+" / 05";
+		}else{
+			i=0;
+			s=i+1;
+			 document.getElementById("slide-id").innerHTML ="0"+s+" / 05";
+			document.slide.src=images[i];
+		}
+	})
+	
+	$('section').on('click','#right',function(){	
+		if(i<images.length-1){
+			i++;
+			s=i+1;
+			 document.getElementById("slide-id").innerHTML ="0"+s+" / 05";
+			document.slide.src=images[i];
+		}else{
+			i=0;
+			s=i+1;
+			 document.getElementById("slide-id").innerHTML ="0"+s+" / 05";
+			document.slide.src=images[i];
+		}
+	})
+
+	images2=[
+	'../MongoDB/style/img/paint1.png',
+	'../MongoDB/style/img/paint2.png'
+	];
+
+	var j=images2.length;
+	document.slide1.src=images2[0];
+	document.slide2.src=images2[1];
+	$('section').on('click','#left1',function(){
+		
+		if(i>images2.length-1){
+			i--;
+			document.slide1.src=images2[i];
+			document.slide2.src=images2[i+1];
+		}else{
+			i=0;
+			document.slide1.src=images2[i];
+			document.slide2.src=images2[i+1];
+		}
+	})
+	
+	$('section').on('click','#right1',function(){	
+		if(j<images2.length-1){
+			j++;
+				console.log("hs2 "+j);
+				document.slide1.src=images2[j-1];
+				document.slide2.src=images2[j];
+			j++;
+		}else{
+			console.log("hs "+j);
+			document.slide1.src=images2[j-1];
+			document.slide2.src=images2[j-2];
+			j=j-2;
+		}
+	})	
